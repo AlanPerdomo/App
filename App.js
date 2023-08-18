43b78c07a2f0513fc9f91d982851cfd83ed2f257
= <=>
2023-08-18 10:28:36 -0300

    .

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Image } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [matricula, setMatricula] = useState("Matricula");
+  const [nome, setNome] = useState("Nome");
+  const matInteira = () => {
+    setMat(parseInt(matricula));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.display}>Sistema de cadastramento de clientes.</Text>
+      <Text style={styles.display1}>Entrada de dados.</Text>
+      <Text style={styles.display2}>Matricula</Text>
+
+      <TextInput
+        style={styles.matricula}
+        value={String(matricula)}
+        onChangeText={(texto) => setMatricula(texto)}
+      ></TextInput>
+
+      <Text style={styles.display2}>Nome</Text>
+
+      <TextInput
+        style={styles.nome}
+        value={String(nome)}
+        onChangeText={(texto) => setNome(texto)}
+      ></TextInput>
+
+      <Image
+        style={styles.imagem}
+        resizeMode="stretch"
+        source={require("./image/Heimer.png")}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +43,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#aaa",
+  },
+  display: {
+    marginLeft: 20,
+    marginTop: 50,
+  },
+  display1: {
+    marginLeft: 60,
+  },
+  matricula: {
+    backgroundColor: "#FFF",
+    borderEndWidth: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 10,
+    padding: 2,
+  },
+  nome: {
+    backgroundColor: "#FFF",
+    borderEndWidth: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 10,
+    padding: 2,
+  },
+  imagem: {
+    width: "auto",
+    height: 500,
+    margin: 10,
   },
 });
