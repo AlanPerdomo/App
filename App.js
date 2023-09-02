fdb50649681e588ea698876e32b6437c1dca04a7
AlanPerdomo <alanperdomo@hotmail.com>
2023-09-01 21:01:53 -0300

    .

--- App.js
+++ App.js
@@ -1,12 +1,75 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import Tela02 from './Tela02';
-const App = () => { 
-return (
-    <NavigationContainer>
-        {/* Aqui você pode configurar suas rotas usando react-navigation */}
-        <Tela02 />
-    </NavigationContainer>
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  SectionList,
+  StatusBar,
+} from "react-native";
+
+const DATA = [
+  {
+    title: "Pratos Principais",
+    data: ["Filé de peixe", "Frango grelhado", "Risoto de camarão"],
+  },
+  {
+    title: "Sobremesas",
+    data: ["Sorvete", "Torta de morango", "Salada de frutas"],
+  },
+  {
+    title: "Bebidas",
+    data: ["Água mineral", "Coca cola", "Cerveja"],
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.titulo}>CARDÁPIO - Prof. Ediberto</Text>
+        <SectionList
+          sections={DATA}
+          keyExtractor={(item, index) => item.title + index.toString()}
+          renderItem={({ item }) => <Item title={item} />}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text style={styles.header}>{title}</Text>
+          )}
+        />
+      </SafeAreaView>
     );
-};
-export default App; 
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+  },
+  titulo: {
+    fontSize: 40,
+    backgroundColor: "#fff",
+    textAlign: "center",
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+  },
+});
+
+export default App;
